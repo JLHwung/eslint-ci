76a67dcd1ad373ff7699dd845bffd7e990efd090
Andrey Sitnik <andrey@sitnik.ru>
2017-11-20 00:38:06 +0300

    Fix ESLint uppercase

--- bin.js
+++ bin.js
@@ -7,5 +7,5 @@ if (ciJobNumber() === 1) {
   require("eslint/bin/eslint");
 } else {
   //eslint-disable-next-line no-console
-  console.warn("To speed up CI eslint runs only in first job");
+  console.warn("To speed up CI ESLint runs only in first job");
 }
